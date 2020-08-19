f05d4d67b4c3fd13152a80a8939030d43c406692
Vincent Brandon <vbrandon@utah.gov>
2020-08-19 16:16:12 -0600

    Minor fixes and added version file

--- translator/utils.py
+++ translator/utils.py
@@ -1,9 +1,15 @@
 # utils.py
-
+import numpy as np
 
 def check_datetime(values):
     return False
 
 
 def utf8len(s):
-    return len(s.encode('utf-8'))
+    try:
+        return len(s.encode('utf-8'))
+    except Exception as e:
+        if np.isnan(s):
+            return 0
+        else:
+            return len(str(s).encode('utf-8'))
