d4183075da3c77daf95d4166999887a9817d9336
Vincent Brandon <vbrandon@utah.gov>
2020-09-07 09:31:18 -0600

    ADD: added profiler test module

---
+++ tests/test_profiler/test_utils.py
@@ -0,0 +1,21 @@
+#test_utils.py (profiler)
+
+import unittest
+import os
+import logging
+
+# Config
+from tests import config, setup
+
+# User Libs
+from profile.utils import *
+
+
+class TestProfilerUtils(unittest.TestCase):
+    
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
