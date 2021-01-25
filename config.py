bc8fe405509ef1782b26c2235a729656405c3d97
Vincent Brandon <vbrandon@utah.gov>
2021-01-25 08:58:35 -0700

    ENH: added extra handler for stdout

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging 
 
 
@@ -11,4 +12,12 @@ def setup(log=True, **kwargs):
     if 'logpath' in kwargs:
         config['logfile'] = os.path.join(kwargs['logpath'], 'pack.log')
     if log:
-        logging.basicConfig(filename=config['logfile'], filemode='w', level=logging.DEBUG)
+        handlers = [
+            logging.FileHandler(filename=config['logfile'],mode='w'),
+            logging.StreamHandler(sys.stdout)
+        ]
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+            handlers=handlers
+            )
